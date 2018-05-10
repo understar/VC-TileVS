04b4af52bc6bfa56507b238d984484d574921b88
zsy-company <450731921@qq.com>
2018-05-10 17:25:44 +0800

    将TiledVS（一个快速可视域分析算法）迁移到VC++上

---
+++ ViewShed/ViewShed/stdafx.h
@@ -0,0 +1,40 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
+#include "unistd.h"
+#include "lz4.h"
+#include "lz4frame.h"
+#include "lz4frame_static.h"
+#include "lz4hc.h"
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <cassert>
+#include <time.h>
+#include <new>
+#include <iomanip>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+#include <malloc.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <fstream>
+#include <string>
+#include <windows.h>
